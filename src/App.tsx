a058d69b0e2bb1f783347d9d32c43e4d0a7fec5d
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2021-01-07 13:04:22 +0100

    Implement redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,30 @@ import {Provider} from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import {createStackNavigator} from '@react-navigation/stack';
 import DetailsScreen from './screens/DetailsScreen';
-import store from './store';
+import {persistor, store} from './store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{title: 'Welcome'}}
-          />
-          <Stack.Screen
-            name="Details"
-            component={DetailsScreen}
-            options={{title: 'Details'}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PersistGate persistor={persistor} loading={null}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{title: 'Welcome'}}
+            />
+            <Stack.Screen
+              name="Details"
+              component={DetailsScreen}
+              options={{title: 'Details'}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,34 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+import {persistReducer, persistStore} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist/es/constants';
 import rootReducer from './rootReducer';
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
+const persistor = persistStore(store);
+
 export type AppDispatch = typeof store.dispatch;
-export default store;
+export {store, persistor};
