a63a454c8e1dff39fea8a48b9ba5b4ba088b9df4
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2021-01-07 12:17:20 +0100

    Style comments section

--- src/componets/Comment.tsx
+++ src/componets/Comment.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {Comment as CommentType} from '../types';
+import {formatted} from '../utils/date';
 
 export interface Props {
   comment: CommentType;
@@ -8,10 +9,36 @@ export interface Props {
 
 const Comment: React.FC<Props> = ({comment}) => {
   return (
-    <View>
-      <Text>{comment.content}</Text>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text>{comment.content}</Text>
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.detailItem}>{comment.author}</Text>
+        <Text style={styles.detailItem}>{formatted(comment.created_at)}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    marginBottom: 30,
+    paddingBottom: 5,
+  },
+  content: {
+    paddingBottom: 10,
+  },
+  details: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  detailItem: {
+    fontSize: 12,
+    color: '#666',
+  },
+});
 
 export default Comment;
